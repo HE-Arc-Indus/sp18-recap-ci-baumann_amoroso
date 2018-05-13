e6784477d6e640ddfdfeace7fdcf01d965c24ccc
sebbmn <sebbmn@gmail.com>
2018-05-13 13:52:34 +0200

    add empty test methods

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,8 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-class FizzBuzzTest {
+/**
+ *
+ * @author sebastie.baumann
+ */
+public class FizzBuzzTest {
+  private FizzBuzz instance;
+  public FizzBuzzTest() {
 
-  @org.junit.jupiter.api.BeforeEach
-  void setUp() {
+  }
+  @Before
+  public void setUp() {
+    instance = new FizzBuzz();
+  }
+  @Test public void listeDeUnACent() {
+
+  }
+  @Test public void testFizz() {
+
+  }
+  @Test public void testBuzz() {
+
+  }
+  @Test public void testFizzBuzz() {
+
   }
 }
