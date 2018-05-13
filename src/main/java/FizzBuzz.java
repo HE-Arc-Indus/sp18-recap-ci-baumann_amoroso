4fc6de209d6382aa902cfd87e5c837eca0019c1b
sebbmn <sebbmn@gmail.com>
2018-05-13 13:55:07 +0200

    add empty FizzBuzz methods

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,3 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sebastie.baumann
+ */
 public class FizzBuzz {
+  public FizzBuzz() {
+    // TODO code application logic here
+  }
+  public int[] cheatSheet(){
+    return null;
+  }
+  public void display() {
 
+  }
+  public String displayFizz(int val) {
+    return null;
+  }
+  public String displayBuzz(int val) {
+    return null;
+  }
+  public String displayFizzBuzz(int val) {
+    return null;
+  }
 }
