914ec6ff4afc55b5de4d8337382b12c023e7cd78
Francesco <francesco.amoroso@he-arc.ch>
2018-05-14 11:18:41 +0200

    Add test for fizzbuzz

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author sebastie.baumann
+ * @author francesc.amoroso
  */
 public class FizzBuzzTest {
   private FizzBuzz instance;
@@ -21,15 +22,24 @@ public class FizzBuzzTest {
     instance = new FizzBuzz();
   }
   @Test public void listeDeUnACent() {
-
+    int[] tab = instance.cheatSheet();
+    for(int i=0;i<100;i++){
+      assertEquals(i+1,tab[i]);
+    }
   }
   @Test public void testFizz() {
-
+    for(int i=0;i<100;i++){
+      if((i+1)%3 ==0)assertEquals("Fizz",instance.displayFizz(i+1));
+    }
   }
   @Test public void testBuzz() {
-
+    for(int i=0;i<100;i++){
+      if((i+1)%5 == 0)assertEquals("Buzz",instance.displayBuzz(i+1));
+    }
   }
   @Test public void testFizzBuzz() {
-
+    for(int i=0;i<100;i++){
+      if(((i+1)%5 == 0)&&((i+1)%3 == 0))assertEquals("FizzBuzz",instance.displayFizzBuzz(i+1));
+    }
   }
 }
