1ea95de381a4d65835df07ba78a8696f69acd50b
sebbmn <sebbmn@users.noreply.github.com>
2018-05-14 12:15:54 +0200

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fizzbuzz;
+
+/**
+ *
+ * @author sebastie.baumann
+ */
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb.display();
+    }
+}
